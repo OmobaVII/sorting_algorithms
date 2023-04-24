64ff97b49031a41919a908ca0ffd5d58fd28f715
OmobaVII <omoba.career@gmail.com>
2023-04-24 02:28:40 -0700

    Shell_sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * shell_sort - a function that sorts an array of int in ascending order
+ * using shell sort algorithm, via the Knuth sequence
+ * @array: the array of int to sort
+ * @size: the size of the array
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, a, b;
+	int tmp;
+
+	if (array == NULL || size <= 1)
+	{
+		return;
+	}
+	while (gap < size)
+	{
+		gap = gap * 3 + 1;
+	}
+	gap = (gap - 1) / 3;
+	while (gap > 0)
+	{	
+		for (a = gap; a < size; a++)
+		{
+			tmp = array[a];
+			for (b = a; b >= gap && array[b - gap] > tmp; b -= gap)
+			{
+				array[b] = array[b - gap];
+			}
+			array[b] = tmp;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
+	
+}
--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int l_partition(int *array, int low, int high, size_t size);
 void quick_sort_help(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
